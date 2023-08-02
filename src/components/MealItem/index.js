9c020dc331300e133a043ba944d772136cbaa950
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-08-01 22:08:12 -0300

    feat: Adding Images & Styling

--- src/components/MealItem/index.js
+++ src/components/MealItem/index.js
@@ -1,10 +1,72 @@
-import { Text } from "react-native";
-import { View } from "react-native";
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
-export default function MealItem({ title }) {
+export default function MealItem({
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailText}>{duration}m</Text>
+            <Text style={styles.detailText}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailText}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "#FFF",
+    elevation: 4,
+    // ios
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailText: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
