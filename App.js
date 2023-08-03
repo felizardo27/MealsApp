9f9e01123c5bd4e0449fa61f4434e87b1268ad68
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-08-02 21:27:29 -0300

    feat: Styling Screen Headers & Backgrounds

--- App.js
+++ App.js
@@ -12,8 +12,22 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#351401",
+            },
+            headerTintColor: "#FFF",
+            cardStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "All Categories",
+            }}
+          />
           <Stack.Screen name="MealsOverView" component={MealsOverViewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
